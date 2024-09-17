6d0ff7ab239643a15c86cb00f15503a6b0355416
kiran wankhade <kiranwankhade7738@gmail.com>
2024-09-17 18:08:17 +0530

    Added files

---
+++ seeders/20240917094847-demo-employeerole.js
@@ -0,0 +1,23 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('EmployeeRoles', [{
+      employeeId: 1,
+      roleId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },{
+      employeeId: 2,
+      roleId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },{
+      employeeId: 3,
+      roleId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('EmployeeRoles', null, {});
+  },
+};
